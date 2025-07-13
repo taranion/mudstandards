35d8962f784ab65b82223469d2ac51688609759a
Stefan Prelle <stefan@rpgframework.de>
2025-07-13 17:20:07 +0200

    Change FavIcon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
         title: 'MUD Standards',
         logo: {
           alt: 'Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/Logo.png',
         },
         items: [
           {
