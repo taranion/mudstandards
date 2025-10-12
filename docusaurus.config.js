48a805a44a55cfcd94fc5db797e9253e5e6c3cd3
Stefan Prelle <stefan@rpgframework.de>
2025-10-12 20:20:16 +0200

    Change base url back

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,15 +23,15 @@ const config = {
   url: 'http://mudstandards.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/home/prelle/git/MUDStandards/build/index.html/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'taranion', // Usually your GitHub org/user name.
   projectName: 'mudstandards', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenAnchors: 'ignore',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -39,11 +39,6 @@ const config = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
-  },
-  markdown: {
-    hooks: {
-        onBrokenMarkdownLinks: 'warn'
-    }
   },
 
   presets: [
